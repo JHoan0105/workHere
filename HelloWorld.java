c23c3e405d9ee8fe3b58bd98772b5f6f00e9319f
JHoan0105 <80090331+JHoan0105@users.noreply.github.com>
2021-03-05 10:46:36 -0500

    update some files

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+/*
+ * Student Name: Your Name Here
+ * Lab Professor: Professor Name Here
+ * Due Date: Due Date Here
+ * Modified: When the file was created or last modified
+ * Description: A brief description of what the class does “this class outputs a
+ *              greeting from within method main.
+ */
+public class HelloWorld{ // start class body
+    /* This is the entry point for the program*/
+    public static void main(String[] args){ // start method main
+        System.out.println("Hello World"); // output "Hello World"
+    } // stop method main
+} // end class body
